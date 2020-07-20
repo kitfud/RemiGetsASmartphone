bad36567639afc3242efcffbf6827747fe0fe7d8
Kit Fuderich <kitfuderich@gmail.com>
2020-07-20 16:01:53 -0700

    react router implemented for changing pages

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,21 @@
 import React, {Component} from 'react'
-import Home from './HomeComponent.js'
+import Home from './HomeComponent.js';
+import About from './AboutComponent.js';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
     render(){
         return(
 <React.Fragment>
 <Header/>
-<Home/>
+
+<Switch>
+<Route path='/home' component={Home} />
+<Route path='/about' component={About} />
+</Switch>
+
 <Footer/>
 </React.Fragment>            
 
