d6c8dbd65f00ab4e350896d633fbe8017f7427ce
Kit Fuderich <kitfuderich@gmail.com>
2020-07-21 13:46:29 -0700

    connected to back end firebase server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,13 +1,24 @@
 import * as ActionTypes from './ActionTypes';
-import {SLIDES} from '../shared/slides';
+import { auth, firestore, fireauth, firebasestore,storage } from '../firebase/firebase';
+//import {SLIDES} from '../shared/slides';
+import firebase from 'firebase';
 
 export const fetchSlides = () => (dispatch) => {
 
     dispatch(slidesLoading(true));
-
-    setTimeout(() => {
-        dispatch(addSlides(SLIDES));
-    }, 2000);
+    return firestore.collection('slides').get()
+    .then(snapshot => {
+        let slides = [];
+        snapshot.forEach(doc => {
+            const data = doc.data()
+            const _id = doc.id
+            slides.push({_id, ...data });
+        });
+        return slides;
+    })
+    .then(slides => dispatch(addSlides(slides)))
+    .catch(error => dispatch(slidesFailed(error.message)));
+  
 }
 
 export const slidesLoading = () => ({
